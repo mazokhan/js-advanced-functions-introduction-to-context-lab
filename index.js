c14bbc50c0778a6885b9313dbbe905121430791c
mazen <mak9986@hotmail.com>
2021-11-07 16:05:30 +1100

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
